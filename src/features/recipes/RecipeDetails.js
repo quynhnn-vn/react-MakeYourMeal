19137cf460fbf62e6449d54fe64587ed35e4f921
quynhnn-vn <nnquynh.0111@gmail.com>
2021-07-15 20:14:26 +0200

    Add styles for RecipeDetails

--- src/features/recipes/RecipeDetails.js
+++ src/features/recipes/RecipeDetails.js
@@ -1,3 +1,4 @@
+import "./RecipeDetails.css";
 import React, { useState, useCallback, useEffect } from "react";
 import { useParams } from "react-router";
 import { getRecipeInfo } from "../../api/api";
@@ -9,12 +10,22 @@ export const RecipeDetails = () => {
   const [recipe, setRecipe] = useState({});
   const [types, setTypes] = useState([]);
   const [diets, setDiets] = useState([]);
+  const [instructions, setInstructions] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
 
   const renderRecipeInfo = useCallback(() => {
     getRecipeInfo(id).then((recipe) => {
       setRecipe(recipe);
       setTypes(recipe.dishTypes);
       setDiets(recipe.diets);
+      setInstructions(
+        recipe.analyzedInstructions.map((instruction) =>
+          instruction.steps.map((item) => item.step)
+        )
+      );
+      setIngredients(
+        recipe.extendedIngredients.map((ingredient) => ingredient.original)
+      );
     });
   }, [id]);
 
@@ -23,27 +34,66 @@ export const RecipeDetails = () => {
   }, [renderRecipeInfo]);
 
   return (
-    <div>
-      <img src={recipe.image} alt="" />
-      <h2>{recipe.title}</h2>
-      <p>{ReactHtmlParser(recipe.summary)}</p>
-      <div>
-        <div>
-          Tags:
-          {types.map((type) => (
-            <NavLink to={`/tags/${type}/1`}>
-              <h4>{type}</h4>
-            </NavLink>
-          ))}
+    <div className="details">
+      <div className="description-container">
+        <img src={recipe.image} alt="" />
+        <div className="description">
+          <h2>{recipe.title}</h2>
+          <p>{ReactHtmlParser(recipe.summary)}</p>
         </div>
-        <div>
-          Diets:
-          {diets.length === 0 ? "None" : diets.map((diet) => <h4>{diet}</h4>)}
+        <div className="other-info-container">
+          <div className="other-info">
+            <div className="tags">
+              TAGS:{" "}
+              {types.map((type) => (
+                <NavLink to={`/tags/${type}/1`}>
+                  <span>{`#${type}, `}</span>
+                </NavLink>
+              ))}
+            </div>
+            <div>
+              DIETS:{" "}
+              {diets.length === 0
+                ? "None"
+                : diets.map((diet) => <span>{`${diet}, `}</span>)}
+            </div>
+          </div>
+          <div className="other-info">
+            <div>
+              READY IN: <span>{recipe.readyInMinutes} minutes</span>
+            </div>
+            <div>
+              SERVINGS: <span>{recipe.servings} persons</span>
+            </div>
+          </div>
+          <div className="other-info">
+            <div>
+              SPOONACULAR SCORE: <span>{recipe.spoonacularScore}</span>
+            </div>
+            <div>
+              HEALTH SCORE: <span>{recipe.healthScore}</span>
+            </div>
+          </div>
         </div>
-        <div>Ready In: {recipe.readyInMinutes} minutes</div>
-        <div>Servings: {recipe.servings} persons</div>
       </div>
-      <p>{ReactHtmlParser(recipe.instructions)}</p>
+      <div className="instruction-container">
+        <div className="ingredient">
+          <h2>INGREDIENTS</h2>
+          <ul>
+            {ingredients.map((ingredient) => (
+              <li>{ingredient}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="instruction">
+          <h2>INSTRUCTIONS</h2>
+          <ul>
+            {instructions.map((instruction) =>
+              instruction.map((step) => <li>{step}</li>)
+            )}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
